bdf84b9919238dfeecf8f6a13f4df428d1730410
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-11 21:23:47 +0300

    1

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваш род деятельности");
+            string profession = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваш возраст");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, Вам {age}, и Вы {profession}");
+        }
+    }
+}
